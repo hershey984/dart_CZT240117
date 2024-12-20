dc35d62041c945219b80526e9f9be0fb55504e40
hershey984 <harshsawant984@gmail.com>
2024-12-20 10:46:39 +0530

    Add file Even Odd

---
+++ 3_checkEvenOdd.dart
@@ -0,0 +1,23 @@
+/*
+Write a Dart program to check if a given number is odd or even.
+
+    Define a variable num = 11.
+    Print "Even" if the number is even.
+    Print "Odd" if the number is odd.
+
+Expected Output:
+
+The number is Odd.
+*/
+
+void main() {
+  int num = 9;
+
+  //conditions for eve odd
+  if(num % 2 == 0) {
+    print("The number is Even");
+  }else {
+    print("The number is Odd");
+  }
+
+}
